badcb0d0a461c316a3d52fe8c5d2cb2393f19311
jonnylin13 <jonnylin13@gmail.com>
2018-03-01 16:23:34 -0800

    prelab

--- Labs/Lab5/router.c
+++ Labs/Lab5/router.c
@@ -6,27 +6,56 @@
 
 typedef struct {
     int size;
-    int *data[1000][1000];
+    int **data;
 } cost_table;
 
 /** Convenience function to return cost at position p[i][j] */
 int get_cost(cost_table *table, int i, int j) {
-    return *table->data[i][j];
+    return table->data[i][j];
+}
+
+/** Runs Djikstra's algorithm to re-calculate least-cost paths */
+void _calculate() {
+
+}
+
+/** Listens for commands and places them into a queue
+ * Every 10-20 seconds (lifecycle of calculate thread)
+ * The program will first execute the next command in the queue
+ * Then it will continue with normal operation
+ * This is to ensure that we are not manipulating our data struct asynchronously from two places
+ */
+void command_listener() {
+    int done = 0;
+    while (done == 0) {
+
+    }
 }
 
 // Multi-threaded functions
 
+/** Sleeps for a random number of seconds (10-20)
+ * Runs Djikstra's Algorithm to calculate least-cost paths
+ * Outputs the current least costs
+*/
 void *calculate(void *arg) {
-
+    // Sleep
+    // Then calculate
+    _calculate();
 }
 
-
+/** Receives messages from other nodes 
+ * When receiving an updated cost c from sender s to this node
+ * c will update in both this node and the sender
+ * At the end, call _calculate()
+ */
 void *recv_update(void *arg) {
-
+    // Init UDP
+    // Recv message in loop
+    // Upon receiving a message, update the cost_table and _calculate()
 }
 
-// Entrypoint
-
+/** Entry point */
 int main(int argc, char* argv[]) {
 
     if (argc != 5) {
@@ -34,19 +63,13 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Input validation
-    char term;
-    if (scanf("%d%c", &argv[1], &term) != 2 || term != '\n') {
-        printf("Invalid router id\n");
-        return 1;
-    } 
-    int rid = *argv[1];
+    // Inputs
+    char rid_c = *argv[1];
+    char n_nodes_c = *argv[2];
+    int rid = rid_c - '0';
+    int n_nodes = n_nodes_c - '0';
 
-    if (scanf("%d%c", argv[2], &term) != 2 || term != '\n') {
-        printf("Invalid number of nodes\n");
-        return 1;
-    }
-    int n_nodes = *argv[2];
+    printf("Creating cost table with %d nodes\n" , n_nodes);
 
     FILE *file1 = fopen(argv[3], "rb");
     FILE *file2 = fopen(argv[4], "rb");
@@ -54,30 +77,49 @@ int main(int argc, char* argv[]) {
     // Initialize our tables
     int cost_a;
     char buff[1];
-    int *data[n_nodes][n_nodes];
+    int **data = (int **) malloc(sizeof(int*) * n_nodes);
+
+    for (int x = 0; x < n_nodes; x++) {
+        data[x] = (int *) calloc(n_nodes, sizeof(int));
+    }
+
     int comp = fread(&buff, 1, 1, file1);
     int i, j = 0;
     while(comp == 1) {
 
-        if (buff[0] == '\n') j++; continue;
-        if (buff[0] == ' ') continue;
+        
+        printf("Test\n");
 
-        data[i][j] = *buff[0];
+        if (buff[0] == '\n') {
+            j++;
+            comp = fread(&buff, 1, 1, file1);
+            continue;
+        }
+        if (buff[0] == ' ') {
+            comp = fread(&buff, 1, 1, file1);
+            continue;
+        }
+
+        data[i][j] = buff[0];
 
         i++;
         comp = fread(&buff, 1, 1, file1);
     }
-    cost_table ct = {.size = n_nodes, .data = data};
+
+    fclose(file1);
+    
+    cost_table ct;
+    ct.size = n_nodes;
+    ct.data = data;
 
     printf("cost: %d\n", ct.data[0][0]);
+
+    return 1;
 
     pthread_t tid;
     pthread_create(&tid, NULL, recv_update, NULL);
     pthread_create(&tid, NULL, calculate, NULL);
-
-    // Command listener
-    int done = 0;
-    while (done == 0) {
+    command_listener();
 
     }
 
